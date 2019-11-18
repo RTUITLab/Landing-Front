ca4a87d259798edf22d8e8636497d90ba3bbb223
Andrew <cuznetsofandrey@yandex.ru>
2019-11-18 22:59:59 +0300

    [ADD] Footer section

---
+++ src/modules/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Footer.css';
+import title from './src/title.png'
+import logo from './src/logo.svg'
+import { Link } from "react-scroll";
+
+const Footer = () => {
+    return (
+        <div className="footer-wrapper">
+            <div className="footer__logo">
+                <img src={logo} alt="" height="54px"/>
+                <img src={title} alt="" className="footer__title"/>
+            </div>
+            <div className="footer__navigation">
+                <div className="row">
+                    <div className="col-3">
+                        <ul className="footer__navigation__item">
+                            <li><Link to="about" spy={true} smooth={true} duration={1000}>О нас</Link></li>
+                            <li>Новости</li>
+                            <li>Устройства</li>
+                            <li><Link to="projects" spy={true} smooth={true} duration={1000}>Проекты</Link></li>
+                            <li>Сотрудники</li>
+                        </ul>
+                    </div>
+                    <div className="col-3">
+                        <ul className="footer__navigation__item">
+                            <li><span className="contacts">Контакты</span></li>
+                            <li><a href="https://vk.com/rtuitlab">vk.com/rtuitlab</a></li>
+                            <li><a href="https://telegram.me/rtuitlab">telegram.me/rtuitlab</a></li>
+                            <li><a href="https://youtube.com/rtuitlab">youtube.com/rtuitlab</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-6">
+                        <ul className="footer__navigation__item footer__right">
+                            <li>Войти в систему</li>
+                            <br/>
+                            <li>РТУ МИРЭА</li>
+                            <li>Институт Информационных Технологий</li>
+                        </ul>
+                    </div>
+                    <div className="col-12 copyright-wrapper">
+                        <div className="copyright__circle"></div>
+                        <h6 className="copyright">Москва</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
