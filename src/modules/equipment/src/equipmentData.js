d7ec9be94abd94b14815af819a4ac12f968cd265
Maksim <maksalmak@gmail.com>
2021-03-27 14:34:20 +0000

    Update image hosting

--- src/modules/equipment/src/equipmentData.js
+++ src/modules/equipment/src/equipmentData.js
@@ -1,55 +1,55 @@
 const equipmentData = [
     {
         title: 'Microsoft HoloLens',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/hololens1.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/hololens1.png',
     },
     {
         title: 'Microsoft HoloLens 2',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/hololens2.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/hololens2.png',
     },
     {
         title: 'HTC Vive Cosmos',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/htc_vive_cosmos.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/htc_vive_cosmos.png',
     },
     {
         title: 'HTC Vive Pro',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/htc_vive_pro.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/htc_vive_pro.png',
     },
     {
         title: 'HTC Vive',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/htc_vive.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/htc_vive.png',
     },
     {
         title: 'Oculus Quest',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/oculus_quest.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/oculus_quest.png',
     },
     {
         title: 'Oculus Rift',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/oculus_rift.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/oculus_rift.png',
     },
     {
         title: 'EPOC Emotic',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/epoc_emotiv.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/epoc_emotiv.png',
     },
     {
         title: 'EPOC Flex',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/emotiv_flex.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/emotiv_flex.png',
     },
     {
         title: 'DEUS Odin Pre',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/deus_odin_vr.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/deus_odin_vr.png',
     },
     {
         title: 'Epson Moverio BT-350',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/epson_bt_350.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/epson_bt_350.png',
     },
     {
         title: 'Magic Leap One',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/magic_leap_one.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/magic_leap_one.png',
     },
     {
         title: 'ODG R-7',
-        image: 'https://files.rtuitlab.ru/landing_src/equipment/odg_r7.png',
+        image: 'https://files.rtuitlab.dev/landing_src/equipment/odg_r7.png',
     },
 ]
 export default equipmentData;
