5f50dee976f5885104a0ceebaa70ba47a65810a5
Kirill <kirill.baikin@gmail.com>
2022-05-06 14:56:57 +0300

    Bug fixes
    Основные изменения:
    - Чувствительность галереи проектов на телефонах стала меньше
    - В разделе сотрудников на телефонах, при выборе сотрудника теперь подкручивает к нему

--- src/js/staff/staff.js
+++ src/js/staff/staff.js
@@ -7,13 +7,26 @@ function initStaffSwiper(){
 
   if (!staffSwiper) {
     new Promise((_) => {
-      new Swiper('.staff__swiper', {
-        spaceBetween: 10, slidesPerView: 'auto', freeMode: true, loop: true, loopFillGroupWithBlank: true, autoplay: {
+      let staffSwiper = new Swiper('.staff__swiper', {
+        spaceBetween: 0, slidesPerView: 'auto', freeMode: true, loop: true, loopFillGroupWithBlank: true, autoplay: {
           delay: 0, disableOnInteraction: false, pauseOnMouseEnter: true,
         }, speed: 2200, mousewheel: {
           releaseOnEdges: true,
         },
       });
+
+      globalThis.staffSwiper=staffSwiper;
+
+      let nodes=Array.from(document.getElementById("staffSwiperParent").children)
+
+      // Зачем?
+      // Прикол в том, что свайпер утраивает количество элементов в своем контейнере
+      // То есть если у нас 50 сотрудников, то будет 150 карточек (по три на сотрудника)
+      // Установка inSwiperId нужна для корректного подкручивания к определенной карточке.
+      for(let i in nodes){
+        nodes[i].children[0].setAttribute("inSwiperId", i)
+      }
+
       staffSwiper=true
       _();
     });
