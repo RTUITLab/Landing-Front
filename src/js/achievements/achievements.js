66bd75d89f01f3d83277ff334c4658b2285b9dd7
Kirill <kirill.baikin@gmail.com>
2022-06-02 19:09:11 +0300

    Some improvements
    - Added placeholder to staff images
    - Achievement`s swiper fixes

--- src/js/achievements/achievements.js
+++ src/js/achievements/achievements.js
@@ -5,8 +5,15 @@ function initAchievements() {
   if (!window.Swiper) return;
 
   if (!achievementsSwiper) {
+    if(window.innerWidth<1600){
+      let arr = Array.from(document.getElementsByClassName("hidden-slide"))
+      arr.forEach((e)=>{
+        e.style.display="none"
+      })
+    }
     new Promise((_) => {
       new Swiper('.achievements__swiperParent', {
+        centeredSlides:window.innerWidth<1600,
         slidesPerView: 'auto', spaceBetween: 15, pagination: {
           el: '.achievements__swiperPagination', clickable: true,
         }, mousewheel: {
@@ -16,6 +23,7 @@ function initAchievements() {
       achievementsSwiper = true;
       _();
     });
+
     window.removeEventListener('load', initAchievements);
   }
 }
